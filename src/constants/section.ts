33d6c6954891745b98354f8e7b1d160cb4aa5a97
LeeMir <manofgodmj@gmail.com>
2022-04-27 18:39:12 +0900

    [Feat] section 관련 constant 및 atom 생성

---
+++ src/constants/section.ts
@@ -0,0 +1,23 @@
+export default {
+  intro: 0,
+  about: 1,
+  experience: 2,
+  project: 3,
+  error: 4
+};
+
+export const sectionArr = [
+  'intro',
+  'about',
+  'experience',
+  'project',
+  'error'
+];
+
+export const sectionPos = [
+  { x: 0, y: 0 }, // intro
+  { x: 100, y: 0 }, // about
+  { x: 0, y: 100 }, // experience
+  { x: 100, y: 100 }, // project
+  { x: null, y: null } // error
+];
