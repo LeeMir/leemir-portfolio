26f1028a086bab31f881c86e3c0d4c05f9fc3e69
LeeMir <manofgodmj@gmail.com>
2022-05-18 21:37:52 +0900

    [Chore] 스크롤바 제거

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,13 @@ const GlobalStyle = createGlobalStyle`
   ${reset}
   html {
     font-size: calc(0.35vw + 8.74px);
+    * {
+      -ms-overflow-style: none; /* IE and Edge */
+      scrollbar-width: none; /* Firefox */
+      &::-webkit-scrollbar {
+        display: none; /* Chrome, Safari, Opera*/
+      }
+    }
   }
   span {
     font-family: 'BMHannaAir', sans-serif;
