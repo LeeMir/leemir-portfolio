652b3429c9ef0b17413473f5742831ffa3bfc01b
LeeMir <manofgodmj@gmail.com>
2022-05-20 02:30:39 +0900

    [Chore] 필요 없는 부분 없앰

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 
 import { sectionState } from '../../stores/section';
