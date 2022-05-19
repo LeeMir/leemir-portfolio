6d4e621bfa0b835ba38894c56870cc653dfeaddd
LeeMir <manofgodmj@gmail.com>
2022-05-20 02:30:13 +0900

    [Fix] Recoil 관련 오류 해결

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { RecoilRoot } from 'recoil';
 import AboutMe from '../components/AboutMe';
 import Intro from '../components/Intro';
 import Layout from '../components/Layout';
@@ -6,12 +7,14 @@ import Section from '../components/Section';
 
 const IndexPage = () => {
   return (
-    <Layout>
-      <Intro />
-      <Section>B</Section>
-      <AboutMe />
-      <Section>C</Section>
-    </Layout>
+    <RecoilRoot>
+      <Layout>
+        <Intro />
+        <Section>B</Section>
+        <AboutMe />
+        <Section>C</Section>
+      </Layout>
+    </RecoilRoot>
   )
 }
 
