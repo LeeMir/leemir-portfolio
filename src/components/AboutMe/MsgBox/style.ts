d7e38b480c58d1769e3d95d8459a18ae26c079b4
LeeMir <manofgodmj@gmail.com>
2022-05-20 03:03:52 +0900

    [Design] AboutMe hover 및 이모지 추가

--- src/components/AboutMe/MsgBox/style.ts
+++ src/components/AboutMe/MsgBox/style.ts
@@ -6,8 +6,15 @@ export const Wrapper = styled.div`
   padding: 0.75rem 1rem;
   line-height: 1.5rem;
   border-radius: 3px;
-  font-family: 'BMHannaAir';
+  font-family: 'BMHannaAir', sans-serif, 'TossFaceFontMac';
   font-size: 1rem;
   background-color: ${color.white};
   box-shadow: 0 4px 5px rgba(0,0,0,.5);
+  transition: 0.6s;
+
+  &:hover {
+    transform: translate(0, -10%);
+    background-color: rgba(0, 0, 0, 0.829);
+    color: ${color.white};
+  }
 `;
